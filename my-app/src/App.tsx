ddc05af911fcbf910cd607970af3b4bd457fded6
czhou578 <colizu2020@gmail.com>
2021-12-30 14:02:37 -0800

    finished first load popup

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { StartingModal } from './components/StartingModal';
 
 function App() {
+  const [showBoard, setShowBoard] = useState(false);
+
   return (
     <div className="App">
-      
+      {!showBoard ? <StartingModal showBoard={showBoard} showBoardHandler={setShowBoard}/> : <div> colin </div>}
     </div>
   );
 }
