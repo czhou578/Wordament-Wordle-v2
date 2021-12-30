085059decc01a79e14b1809e433438d2ad4e2b15
czhou578 <colizu2020@gmail.com>
2021-12-29 21:17:48 -0800

    set up webpack

--- my-app/webpack.config.js
+++ my-app/webpack.config.js
@@ -1,2 +1,44 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
+module.exports = {
+  mode: "development",
+  devServer: {
+    compress: true,
+    port: 3000,
+    historyApiFallback: true
+  },
+  entry: __dirname + '/src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx$?/,
+        use: ['ts-loader']
+      },
+      {
+        test: /\.css$?/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devTool: 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.tsx', 'ts']
+  },
+  target: 'web',
+  output: {
+    filename: '[name][contenthash].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebPackPlugin({template: './public/index.html'})
+  ]
+}
