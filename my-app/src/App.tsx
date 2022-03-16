ee4f8dd44161834af8638bcbe2824bca6364a46a
czhou578 <colizu2020@gmail.com>
2022-03-15 17:29:29 -0700

    testing out toast

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,22 +5,52 @@ import Clock from "./components/Clock";
 import styles from "./app.module.css";
 import ScoreBoard from "./components/ScoreBoard";
 import EndGameModal from "./components/EndGameModal";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [showBoard, setShowBoard] = useState(false);
   const [timesUp, setTimesUp] = useState(false);
   const [score, setScore] = useState(0);
 
+  const notify = () => toast("Welcome to the Game");
+
   return (
     <div className={styles.appContainer}>
       <div>
         {!showBoard ? (
-          <StartingModal
-            showBoard={showBoard}
-            showBoardHandler={setShowBoard}
-          />
+          <>
+            <StartingModal
+              showBoard={showBoard}
+              showBoardHandler={setShowBoard}
+            />
+            <ToastContainer
+              position="bottom-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />          
+          </>
         ) : timesUp ? (
-          <EndGameModal score={score} setTimesUp={setTimesUp} />
+          <>
+            <EndGameModal score={score} setTimesUp={setTimesUp} />
+            <ToastContainer
+              position="bottom-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />
+          </>
         ) : (
           <div>
             <Clock setTimesUp={setTimesUp} />
